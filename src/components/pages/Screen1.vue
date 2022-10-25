6e7ea05af7107308155369749edce7c1023ca629
NUCAL <selinyksel9@gmail.com>
2022-10-25 12:07:16 +0300

    NUCAL-UI | Front-end application initial commit, powered by NUCAL development team.

---
+++ src/components/pages/Screen1.vue
@@ -0,0 +1,55 @@
+<template>
+  <NucGridPage v-bind:gap="{'col':0,'row':0}" v-bind:rows="[1]"
+    v-bind:schema="{'r0-c0':{'col':0,'row':0,'showIn':['xs','sm','md','lg'],'colSpan':1,'rowSpan':1}}"
+    v-bind:columns="[1]" overflow="auto" v-bind:designMode="false" v-bind:minNsLimit="3759"
+    v-bind:style="{'background':'#FFFFFF','paddingTop':'0px','paddingLeft':'0px','paddingRight':'0px','paddingBottom':'0px'}">
+    <NucGridCell v-bind:wrap="true" v-bind:reverse="false" overflow="hidden" direction="column"
+      v-bind:showInDevice="['xs','sm','md','lg']" verticalAlignment="top" horizontalAlignment="left"
+      id="8c1e2245-2da1-4a70-bece-de421f6b267c" v-bind:col="0" v-bind:row="0" v-bind:colSpan="1" v-bind:rowSpan="1"
+      v-bind:marginTop="0" v-bind:marginLeft="0" v-bind:marginRight="0" v-bind:marginBottom="0"
+      v-bind:style="{'height':'auto','background':'transparent','paddingTop':'0px','borderColor':'','borderStyle':'solid','borderWidth':'0px','paddingLeft':'0px','borderRadius':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','borderLeftWidth':'0px','borderRightWidth':'0px','borderBottomWidth':'0px','borderTopLeftRadius':'1px','borderTopRightRadius':'1px','borderBottomLeftRadius':'1px','borderBottomRightRadius':'1px'}"
+      slot="r0-c0" area="r0-c0">
+      <NucGroupElement top="-0.2631578947368421%" left="0%" v-bind:snapX="false" v-bind:snapY="true"
+        v-bind:isLock="true" bgColor="" position="absolute" ncTooltipText="" id="076a889a-dcc8-499f-8fa0-3fff81cba552"
+        v-bind:style="{'color':'#FFFFFF','width':'2484.0908219999997px','height':'1353.989418px','zIndex':2,'fontSize':'26.653335000000002px','maxWidth':'','minWidth':'17.76889','tabIndex':'-1.776889','boxShadow':'','fontStyle':'normal','marginTop':'0px','maxHeight':'','minHeight':'17.76889','textAlign':'center','background':'','fontFamily':'Roboto-Regular,Roboto','fontWeight':'normal','lineHeight':'2.22111125','marginLeft':'0px','paddingTop':'','textShadow':'','visibility':'visible','borderColor':'#767676','borderStyle':'solid','marginRight':'0px','paddingLeft':'','marginBottom':'0px','paddingRight':'','paddingBottom':'','borderTopWidth':'1.776889px','textDecoration':'none','borderLeftWidth':'1.776889px','borderRightWidth':'1.776889px','borderBottomWidth':'1.776889px','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px'}">
+        <WImage top="6.824146981627297%" left="0.1430615164520744%" v-bind:blank="false" v-bind:snapX="true"
+          v-bind:snapY="false" v-bind:rounded="false" stretch="none" imageUrl="https://www.linkpicture.com/q/j_11.jpg"
+          position="absolute" v-bind:isEnabled="true" v-bind:thumbnail="false" blankColor="black"
+          v-bind:aspectRatio="true" dataLoading="static" ncTooltipText="Please write a text to display in tooltip..."
+          v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':true,'structure':[{'type':'string','update':{'type':'dataModelForDynamicType'},'propName':'imageUrl','displayName':'Value'}]}"
+          ncClickBorderColor="black" ncHoverBorderColor="black" dataModelForDynamicType="string"
+          id="84c0b3b7-c2f0-47b5-9dbe-b88849ba66cc"
+          v-bind:style="{'width':'3759.8971239999996px','height':'1016.380508px','zIndex':4,'maxWidth':'','minWidth':'','boxShadow':'','marginTop':'0px','maxHeight':'','minHeight':'','marginLeft':'0px','paddingTop':'0px','textShadow':'','visibility':'visible','borderColor':'#4B74FF','borderStyle':'solid','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','borderLeftWidth':'0px','borderRightWidth':'0px','borderBottomWidth':'0px','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px'}"/>
+        <WLabel top="34.38320209973753%" left="50.78683834048641%"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in orci magna. Donec commodo, eros eget hendrerit bibendum, diam tellus porttitor nisi, ut tristique orci diam sed neque. Nulla ornare nulla id odio faucibus, id tempor nulla vehicula. Aliquam ut interdum odio. Duis congue eleifend orci quis volutpat. Nam finibus condimentum turpis, pretium tempus felis consectetur non"
+          v-bind:snapX="false" v-bind:snapY="false" theme="theme--light01" position="absolute" ncTypography="custom"
+          ncTooltipText=""
+          v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':false,'structure':[{'type':'string','propName':'text','displayName':'Value'}]}"
+          id="f29fa03d-2ad4-4abf-be1e-6dd00e2792ff"
+          v-bind:style="{'color':'#3f3e3e','width':'1105.224958px','height':'342.939577px','zIndex':7,'fontSize':'35.53778px','maxWidth':'','minWidth':'','fontStyle':'normal','marginTop':'0px','maxHeight':'','minHeight':'','textAlign':'left','fontFamily':'OpenSans-Regular,Open Sans','fontWeight':'normal','lineHeight':'1.8657334499999993','marginLeft':'0px','paddingTop':'0px','textShadow':'','visibility':'visible','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','textDecoration':'none'}"/>
+        <WLabel top="26.902887139107612%" left="50.64377682403433%" text="Meet with Business Professionals"
+          v-bind:snapX="false" v-bind:snapY="false" theme="theme--light01" position="absolute" ncTypography="custom"
+          ncTooltipText=""
+          v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':false,'structure':[{'type':'string','propName':'text','displayName':'Value'}]}"
+          id="6768dcbf-6e51-498f-a9e3-1cc78e6fb226"
+          v-bind:style="{'color':'#002a57','width':'835.13783px','height':'62.191114999999996px','zIndex':8,'fontSize':'49.752891999999996px','maxWidth':'','minWidth':'','fontStyle':'normal','marginTop':'0px','maxHeight':'','minHeight':'','textAlign':'left','fontFamily':'OpenSans-SemiBold,Open Sans','fontWeight':'normal','lineHeight':'1.8657334499999993','marginLeft':'0px','paddingTop':'0px','textShadow':'','visibility':'visible','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','textDecoration':'none'}"/>
+      </NucGroupElement>
+    </NucGridCell>
+  </NucGridPage>
+</template>
+<script>
+export default {
+  name: 'Screen1',
+  data() {
+    return {};
+  },
+  props: {},
+  created() {},
+  mounted() {},
+  computed: {},
+  methods: {},
+  watch: {},
+};
+</script>
+
+<style scoped></style>
